d06026ca7d119b3421b0bfed253ea973901ccd8a
Rohit Kale <ruk22@cam.ac.uk>
2020-05-01 22:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  var musicians = []
-  musicians.push("hello")
   for (let i = 0; i <= musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+    array.push(`${"musicians[i]"} plays ${instruments[i]}`)
   }
   return array
 }
